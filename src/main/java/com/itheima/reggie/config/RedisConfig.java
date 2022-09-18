408af79abe0b3bffcaf0174d40d988249b7604ec
周雨辰 <11603788+zzhouyuchen@user.noreply.gitee.com>
2022-09-18 11:27:49 +0800

    瑞吉

---
+++ src/main/java/com/itheima/reggie/config/RedisConfig.java
@@ -0,0 +1,54 @@
+package com.itheima.reggie.config;
+
+//import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.*;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String,Object>redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        //默认的Key序列化器为：JdkSerializationRedisSerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        return redisTemplate;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory factory){
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        //解决查询缓存转换异常问题
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        //配置序列化（解决乱码问题）
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ZERO)
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer))
+                .disableCachingNullValues();
+
+        RedisCacheManager cacheManager = RedisCacheManager.builder(factory).cacheDefaults(config).build();
+
+        return cacheManager;
+    }
+}
---
+++ src/test/java/com/itheima/RedisDemoApplicationTests.java
@@ -0,0 +1,204 @@
+package com.itheima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.DataType;
+import org.springframework.data.redis.core.*;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest
+class RedisDemoApplicationTests {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    void contextLoads() {
+        //存值
+        redisTemplate.opsForValue().set("city123","beijing");
+
+        //使用redis-cli，查看value：get city123
+    }
+
+    /**
+     * 操作String类型数据
+     */
+    @Test
+    public void testString(){
+
+        //存值: set city123 beijing
+        redisTemplate.opsForValue().set("city123","beijing");
+
+        //也可以定义一个变量
+        //ValueOperations valueOperations = redisTemplate.opsForValue();
+        //存值
+        //valueOperations.set("city123", "beijing");
+
+        //取值: get city123
+        String value = (String) redisTemplate.opsForValue().get("city123");
+        System.out.println(value);
+
+        //存值，同时设置过期时间: setex key1 10 value1
+        redisTemplate.opsForValue().set("key1","value1",10l, TimeUnit.SECONDS);
+
+        //存值，如果存在则不执行任何操作: setnx city1234 nanjing
+        Boolean aBoolean = redisTemplate.opsForValue().setIfAbsent("city1234", "nanjing");
+        System.out.println(aBoolean);
+
+        //删除key
+        redisTemplate.delete("city123");
+    }
+
+    /**
+     * 操作Hash类型数据
+     */
+    @Test
+    public void testHash(){
+        HashOperations hashOperations = redisTemplate.opsForHash();
+
+        //存值
+        hashOperations.put("002","name","xiaoming");
+        hashOperations.put("002","age","20");
+        hashOperations.put("002","address","bj");
+
+        //取值
+        String age = (String) hashOperations.get("002", "age");
+        System.out.println(age);
+
+        //删除002中的age
+        //hashOperations.delete("002", "age");
+
+        //获得hash结构中的所有字段
+        Set hashKeys = hashOperations.keys("002");
+        for (Object hashKey : hashKeys) {
+            System.out.println(hashKey + ": " + hashOperations.get("002", hashKey));
+        }
+        //hashKeys.forEach(System.out::println);
+
+        //获得hash结构中的所有值
+        List values = hashOperations.values("002");
+        for (Object value : values) {
+            System.out.println(value);
+        }
+    }
+
+    /**
+     * 操作List类型的数据
+     */
+    @Test
+    public void testList(){
+        ListOperations listOperations = redisTemplate.opsForList();
+
+        //存值
+        listOperations.leftPush("mylist","a");
+        listOperations.leftPushAll("mylist","b","c","d");
+
+        //取值
+        List<String> mylist = listOperations.range("mylist", 0, -1);
+        for (String value : mylist) {
+            System.out.println(value);
+        }
+
+        //获得列表长度 llen
+        Long size = listOperations.size("mylist");
+        int lSize = size.intValue();
+        for (int i = 0; i < lSize; i++) {
+            //出队列
+            String element = (String) listOperations.rightPop("mylist");
+            System.out.println(element);
+        }
+    }
+
+    /**
+     * 操作Set类型的数据
+     */
+    @Test
+    public void testSet(){
+        SetOperations setOperations = redisTemplate.opsForSet();
+
+        //存值
+        setOperations.add("myset","a","b","c","a");
+
+        //取值
+        Set<String> myset = setOperations.members("myset");
+        for (String o : myset) {
+            System.out.println(o);
+        }
+
+        //删除成员
+        setOperations.remove("myset","a","b");
+
+        //取值
+        myset = setOperations.members("myset");
+        for (String o : myset) {
+            System.out.println(o);
+        }
+
+    }
+
+    /**
+     * 操作ZSet类型的数据
+     */
+    @Test
+    public void testZset(){
+        ZSetOperations zSetOperations = redisTemplate.opsForZSet();
+
+        //存值
+        zSetOperations.add("myZset","a",10.0);
+        zSetOperations.add("myZset","b",11.0);
+        zSetOperations.add("myZset","c",12.0);
+        zSetOperations.add("myZset","a",13.0);
+
+        //取值
+        Set<String> myZset = zSetOperations.range("myZset", 0, -1);
+        for (String s : myZset) {
+            System.out.println(s);
+        }
+
+        //修改分数
+        zSetOperations.incrementScore("myZset","b",20.0);
+
+        //取值
+        myZset = zSetOperations.range("myZset", 0, -1);
+        for (String s : myZset) {
+            System.out.println(s);
+        }
+
+        //删除成员
+        zSetOperations.remove("myZset","a","b");
+
+        //取值
+        myZset = zSetOperations.range("myZset", 0, -1);
+        for (String s : myZset) {
+            System.out.println(s);
+        }
+    }
+
+    /**
+     * 通用操作，针对不同的数据类型都可以操作
+     */
+    @Test
+    public void testCommon(){
+        //获取Redis中所有的key
+        Set<String> keys = redisTemplate.keys("*");
+        for (String key : keys) {
+            System.out.println(key);
+        }
+
+        //判断某个key是否存在
+        Boolean itcast = redisTemplate.hasKey("itcast");
+        System.out.println(itcast);
+
+        //删除指定key
+        redisTemplate.delete("myZset");
+
+        //获取指定key对应的value的数据类型
+        DataType dataType = redisTemplate.type("myset");
+        System.out.println(dataType.name());
+
+    }
+}
